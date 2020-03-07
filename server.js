1746b8f0c6f9f2aed2b131347cbfed1f7c4fbd49
Rafael Schloming <rhs@datawire.io>
2020-03-06 20:48:50 -0500

    blah

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http')
 var port = 8080
 
 var server = http.createServer(function (request, response) {
-  console.log("got request!!: " + request.url)
+  console.log("got request!!!: " + request.url)
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.end('Hi AES!\n')
 })
