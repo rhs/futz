e5668606d40744a599f193fe55c8ca7a7509be83
Rafael Schloming <rhs@alum.mit.edu>
2020-03-11 17:33:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = 8080
 var server = http.createServer(function (request, response) {
   console.log("Hi Luke! We got a request: " + request.url)
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Blah!\n')
+  response.end('Blah 2!\n')
 })
 
 server.listen(port)
