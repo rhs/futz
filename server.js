813238c9cc1ba1f3e3093280f2298581a2c9cd28
Rafael Schloming <rhs@alum.mit.edu>
2020-03-12 13:33:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 
 var port = 8080
 
-var server = http.createServer(function (request, response) {
+xvar server = http.createServer(function (request, response) {
   console.log("PR Foo request Log: " + request.url)
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.end('PR Foo\n')
