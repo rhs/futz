f9f99d64c92e8dff62d9ac5447026c58748e9e51
Rafael Schloming <rhs@alum.mit.edu>
2020-04-10 01:23:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = 8080
 var server = http.createServer(function (request, response) {
   console.log("PR Foo request Log: " + request.url)
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('PR Futz 6\n')
+  response.end('PR Futz 7\n')
 })
 
 server.listen(port)
